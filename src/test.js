a86eb4027088b4819ef8814edd5280c36517f07e
Chris Ramer <chrisramer.97@gmail.com>
2021-07-30 12:04:54 -0700

    woot

--- src/test.js
+++ src/test.js
@@ -1,5 +1,51 @@
-function add (num1, num2) {
-	return num1 + num2;
+startingAmount = 4.99;
+quarterCount = 0;
+dimeCount = 0;
+nickelCount = 0;
+pennyCount = 0;
+
+function getQuarters(amount) {
+	if ((amount / 0.25) < 1) {
+		return quarterCount;
+	} else {
+		quarterCount++;
+		startingAmount -= 0.25
+		return getQuarters(startingAmount);
+	}
 }
 
-console.log(add(2, 3));
+function getDimes(amount) {
+	if ((amount / 0.1) < 1) {
+		return dimeCount;
+	} else {
+		dimeCount++;
+		startingAmount -= 0.1;
+		return getDimes(startingAmount);
+	}
+}
+
+function getNickels(amount) {
+	if ((amount / 0.05) < 1) {
+		return nickelCount;
+	} else {
+		nickelCount++;
+		startingAmount -= 0.05;
+		return getNickels(startingAmount);
+	}
+}
+
+function getPennies(amount) {
+	if ((amount / 0.01) < 1) {
+		return pennyCount;
+	} else {
+		pennyCount++;
+		startingAmount -= 0.01;
+		return getPennies(startingAmount)
+	}
+}
+
+console.log("$" + startingAmount + " consists of " +
+	getQuarters(startingAmount) + " quarters, " +
+	getDimes(startingAmount) + " dimes, " +
+	getNickels(startingAmount) + " nickels, " +
+	getPennies(startingAmount) + " pennies");
