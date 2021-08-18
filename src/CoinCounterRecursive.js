a381262b2960fe987b7651444c8fd5b109bc0b5b
Chris Ramer <chrisramer.97@gmail.com>
2021-08-18 12:08:44 -0700

    Improvements

--- src/CoinCounterRecursive.js
+++ src/CoinCounterRecursive.js
@@ -1,42 +1,43 @@
 let startingAmount = 69.420
-let quarters = 0;
-let dimes = 0;
-let nickels = 0;
-let pennies = 0;
 
 console.log(startingAmount + " is comprised of:");
 GetChange(startingAmount);
 
 function GetChange(startingAmount) {
-	if  (startingAmount > 0) {
-		// Quarters
-		if (startingAmount / 0.25 >= 1) {
-			quarters++;
-			startingAmount -= 0.25;
-			GetChange(startingAmount);
-		}
-		// Dimes
-		else if (startingAmount / 0.1 >= 1) {
-			dimes++;
-			startingAmount -= 0.1;
-			GetChange(startingAmount);
-		}
-		// Nickels
-		else if (startingAmount / 0.05 >= 1) {
-			nickels++;
-			startingAmount -= 0.05;
-			GetChange(startingAmount);
-		}
-		// Pennies
-		else if (startingAmount / 0.01 >= 1) {
-			pennies++;
-			startingAmount -= 0.01;
-			GetChange(startingAmount);
+	let quarters = 0;
+	let dimes = 0;
+	let nickels = 0;
+	let pennies = 0;
+
+	Helper(startingAmount)
+
+	function Helper(startingAmount) {
+		if (startingAmount > 0) {
+			// Quarters
+			if (startingAmount / 0.25 >= 1) {
+				quarters++;
+				Helper(startingAmount - 0.25);
+			}
+			// Dimes
+			else if (startingAmount / 0.1 >= 1) {
+				dimes++;
+				Helper(startingAmount - 0.1);
+			}
+			// Nickels
+			else if (startingAmount / 0.05 >= 1) {
+				nickels++;
+				Helper(startingAmount - 0.05);
+			}
+			// Pennies
+			else if (startingAmount / 0.01 >= 1) {
+				pennies++;
+				Helper(startingAmount - 0.01);
+			}
 		}
 	}
-}
 
-console.log(quarters + " quarters");
-console.log(dimes + " dimes");
-console.log(nickels + " nickels");
-console.log(pennies + " pennies");
+	console.log(quarters + " quarters");
+	console.log(dimes + " dimes");
+	console.log(nickels + " nickels");
+	console.log(pennies + " pennies");
+}
